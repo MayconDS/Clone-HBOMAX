5ac66c42552c9c1b3fbca46558d4d686641e224e
Maycon Douglas <crossfireelite601@gmail.com>
2022-08-24 21:09:41 -0300

    inserção engines no package.json

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Series from "../pages/Series";
+import Series from "../pages/series";
 import Home from "../pages/Home";
 import Search from "../pages/Search";
 import Movies from "../pages/Movies";
